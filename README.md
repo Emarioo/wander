45a2520aa6126165b15033f4ad832acf2b784ccd
Emarioo <dataolsson@gmail.com>
2025-04-26 16:03:59 +0200

    Installer backend experimenting

--- README.md
+++ README.md
@@ -3,17 +3,37 @@ Some game with cool installer?
 
 ![](website/public/chatgpt_fantasy_town.png)
 
+This project has 4 applications
+- **Wander** - The game
+- **Installer** - Downloads new versions of the game. Can also automatically download hotfixes and hotreload them while the game is running.
+- **Content Server** - Server where installer downloads game files from.
+- **Website** - Website of the game where you download installer.
+
 # Get started
+Get started by either building project yourself or download the pre-built executables in [Releases](https://github.com/Emarioo/wander/releases).
+
+Once you have the installer you can run it `installer.exe`. It will ask you where to install the game, default is `X`. Then just run the game and have some fun.
+
+If you want to modify the game and have the running game automatically update assets and game code you should overwrite `game.dll`. It you are playing on a server and want everyone to also have their game updated then you would use the installer and content server. Upload your game files through installer. The content server will notify connected installers on your friends computer that a new version is available. If your friends enable auto-update then the installers will download the new game files automatically.
+
+# Building
 **Requirements**
 - Download [BTB](https://github.com/Emarioo/BetterThanBatch) from its releases (or clone and build compiler).
   Put `btb.exe` in your PATH environment variable.
 - Download Visual Studio or GCC (MinGW if on Windows)
 
-**Pre-built**
-Find pre-built executable in [Releases](https://github.com/Emarioo/wander/releases)
+## Installer
+- Build with `build.py installer`
+- Run with `bin\installer.exe`
 
-**Building**
-- Build with `btb src/main.btb -o main.exe`
-- Run with `main.exe`
+## Wander
+- Build with `build.py`
+- Run with `bin\wander.exe`
 
-- Build and run with `btb src/main -r`
+## Website
+- Build with `build.py website`
+- Run with `bin\wander.exe`
+
+## Content Server (for installer)
+- Build with `build.py server`
+- Run with `bin\server.exe`
--- build.py
+++ build.py
@@ -1,26 +1,94 @@
 #!/usr/bin/python3
-import os, sys
+
+import os, sys, hashlib
 
 MODE_COMPILE_GAME = 1
-MODE_RUN_SERVER = 2
+MODE_RUN_WEBSITE = 2
+MODE_COMPILE_INSTALLER = 3
+MODE_RUN_CONTENT_SERVER = 4
+MODE_DISTRIBUTE = 5
 
 def main():
+    version = ""
     mode = MODE_COMPILE_GAME
     argi = 1
     while argi < len(sys.argv):
         arg = sys.argv[argi]
         argi += 1
-        if arg == "server":
-            mode = MODE_RUN_SERVER
+        if arg == "web":
+            mode = MODE_RUN_WEBSITE
+        elif arg == "installer":
+            mode = MODE_COMPILE_INSTALLER
+        elif arg == "server":
+            mode = MODE_RUN_CONTENT_SERVER
+        elif arg == "dist":
+            mode = MODE_DISTRIBUTE
         else:
-            print(f"Unknown argument {arg}")
-            exit(1)
+            if mode == MODE_DISTRIBUTE:
+                if version is None:
+                    version = arg
+                else:
+                    print(f"Version already specified")    
+            else:
+                print(f"Unknown argument {arg}")
+                exit(1)
 
     if mode == MODE_COMPILE_GAME:
-        os.system("btb src/main.btb -r")
-    elif mode == MODE_RUN_SERVER:
-        os.system("btb website/src/website.btb -o bin/website.exe")
-        os.system("bin\\website.exe")
+        compile_game()
+        os.system("bin/core.exe")
+    elif mode == MODE_RUN_WEBSITE:
+        res = os.system("btb website/src/website.btb -o bin/website.exe")
+        if res == 0:
+            os.system("bin\\website.exe")
+    elif mode == MODE_COMPILE_INSTALLER:
+        compile_installer()
+        os.system("bin\\installer.exe")
+    elif mode == MODE_RUN_CONTENT_SERVER:
+        res = os.system("btb installer/src/backend.btb -o bin/server.exe")
+        if res == 0:
+            os.system("bin\\server.exe")
+    elif mode == MODE_DISTRIBUTE:
+        compile_installer()
+        compile_game()
+
+        create_manifest("bin/manifest.txt")
+
+        os.system(f"bin\\installer.exe upload {version}")
+    else: 
+        assert False
+
+def compile_installer():
+    res = os.system("btb installer/src/main.btb -o bin/installer.exe")
+    if res != 0:
+        exit(1)
+
+def compile_game():
+    res = os.system("btb src/main.btb -o bin/core.exe")
+    if res != 0:
+        exit(1)
+
+def create_manifest(output_path):
+    text = ""
+    
+    def add_file(entry_path, path):
+        sha256 = hashlib.sha256()
+        with open(path, 'rb') as f:
+            while chunk := f.read(8192):
+                sha256.update(chunk)
+
+        text.append(f"{entry_path} {sha256.hexdigest()}\n")
+
+    add_file("core.exe", "bin/core.exe")
+    add_file("game.dll", "bin/game.dll")
+
+    for folder, _, files in os.walk("assets"):
+        for filename in files:
+            file_path = os.path.join(folder, filename)
+            print(file_path)
+            add_file(file_path, file_path)
+    
+    with open(output_path, 'w', encoding='utf-8') as manifest:
+        manifest.write(text)
 
 if __name__ == "__main__":
     main()
